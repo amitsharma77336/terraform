e2d7e0db99a5beee2b25a298c846c5588693dacf
Amit Sharma <amitsharma0172@gmail.com>
2023-02-12 21:36:11 +0100

    Added random module

--- Day1/main.tf
+++ Day1/main.tf
@@ -1,10 +1,15 @@
-
+# Edited lines
 
 terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "3.43.0"
+    }
+    random{
+      source = "hashicorp/random"
+      version = "3.4.3"
+
     }
   }
 }
